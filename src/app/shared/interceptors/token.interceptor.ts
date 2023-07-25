ba5030efdd83427a3c1adab9544769f640afcc35
Al1erEgo <al1erego@gmail.com>
2023-07-25 22:02:25 +0200

    fix: tokenInterceptor to use token from authService

--- src/app/shared/interceptors/token.interceptor.ts
+++ src/app/shared/interceptors/token.interceptor.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@angular/core'
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http'
 import { Observable } from 'rxjs'
-import { SessionStorageService } from '../services/session-storage.service'
+import { AuthService } from '../services/auth.service'
 
 @Injectable({ providedIn: 'root' })
 export class TokenInterceptor implements HttpInterceptor {
-  constructor(private readonly sessionStorage: SessionStorageService) {}
+  constructor(private readonly authService: AuthService) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const token = this.sessionStorage.getUserData()?.accessToken
+    const token = this.authService.currentToken
 
     const request = req.clone({
       setHeaders: {
