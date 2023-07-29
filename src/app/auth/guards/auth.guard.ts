37c86c2855ef94d70c7550fa5746d2e13445c836
Al1erEgo <al1erego@gmail.com>
2023-07-29 21:12:29 +0200

    fix: add redirect in auth guard

--- src/app/auth/guards/auth.guard.ts
+++ src/app/auth/guards/auth.guard.ts
@@ -1,14 +1,15 @@
-import { CanActivateFn } from '@angular/router'
+import { CanActivateFn, Router } from '@angular/router'
 import { inject } from '@angular/core'
 import { AuthService } from '../services/auth.service'
 import { NotificationService } from '../../shared/services/notification.service'
 
-//TODO проверять маршрут и кидать на логин если нет текущего маршрута(ввели в адресную строку на пустой вкладке)
-
 export const authGuard: CanActivateFn = () => {
   const authService = inject(AuthService)
   const notificationService = inject(NotificationService)
+  const router = inject(Router)
+
   if (!authService.isAuth) {
+    router.navigate(['/auth'])
     notificationService.handleNotification('To access this page, please login!', 'info')
   }
 
